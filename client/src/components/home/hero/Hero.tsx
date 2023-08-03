ff7ba93216ad274826a71c7307b00d3ed96a8b6b
Alina <alinbogz@gmail.com>
2023-08-03 22:24:24 +0200

    navbar responsive done

--- client/src/components/home/hero/Hero.tsx
+++ client/src/components/home/hero/Hero.tsx
@@ -1,21 +1,24 @@
 import { Link } from "react-router-dom";
-import "./hero.css";
+// import "./hero.css";
+import { Box, Container, Typography } from "@mui/material";
 
 const Hero = () => {
   return (
-    <div className="hero-container">
-      <h1>
+    <Container className="hero-container" sx={{ border: "1px solid red" }}>
+      <Typography>
         Welcome to <br />
-        <span>Food Blog</span>
-      </h1>
-      <p>
-        Expect new recipes weekly, with a mix of savory and sweet including{" "}
-        <em>desserts, breakfasts, entrées, sides, snacks,</em> and more{" "}
-        <Link to="/all-recipes">(find all recipes here).</Link> When you make
-        one of our recipes we want you to say, “How was it that simple but still
-        tastes that good?!”
-      </p>
-    </div>
+        <Typography>Food Blog</Typography>
+      </Typography>
+      <Box>
+        <Typography>
+          Expect new recipes weekly, with a mix of savory and sweet including{" "}
+          <em>desserts, breakfasts, entrées, sides, snacks,</em> and more{" "}
+          <Link to="/all-recipes">(find all recipes here).</Link> When you make
+          one of our recipes we want you to say, “How was it that simple but
+          still tastes that good?!”
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 export default Hero;
--- client/src/components/navbar/NavBar.tsx
+++ client/src/components/navbar/NavBar.tsx
@@ -1,20 +1,39 @@
+import React, { useState } from "react";
 import { styled, Button, IconButton, Badge, BadgeProps } from "@mui/material";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logo.svg";
-import "./NavBar.css";
+import Logo from "../../assets/logo.png";
+
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
-function NavBar() {
+interface Props {
+  window?: () => Window;
+}
+
+const drawerWidth = 240;
+
+function NavBar(props: Props) {
   const user = useSelector((state: RootState) => state.user.user);
   const isLogin = useSelector((state: RootState) => state.user.isLogin);
   const favState = useSelector((state: RootState) => state.favorite.favorites);
   const LoginBTN = styled(Button)({
-    color: "#fff",
-    backgroundColor: "black",
-    border: "1px solid #fff",
-    padding: "5 20px",
+    textDecoration: "none",
+    color: "#3a3939",
+    border: "1px solid grey",
+
     "&:hover": {
       backgroundColor: "#fff",
       color: "black",
@@ -24,43 +43,189 @@ function NavBar() {
 
   const StyledBadge = styled(Badge)<BadgeProps>(() => ({
     "& .MuiBadge-badge": {
-      right: 6,
-      top: 3,
-      padding: "0 4px",
+      backgroundColor: "none",
     },
   }));
+  const linkStyle = {
+    textDecoration: "none",
+    color: "#201f1f",
+  };
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+  const drawer = (
+    <Box
+      style={{ height: "100%" }}
+      onClick={handleDrawerToggle}
+      sx={{ textAlign: "center" }}
+      className="nav-container"
+    >
+      <Typography
+        className="logo-first"
+        textAlign="center"
+        variant="h6"
+        sx={{ my: 2 }}
+      >
+        <span>LOGO</span>
+      </Typography>
+      <Divider />
+      <List>
+        <Link style={linkStyle} to="/">
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText>HOME</ListItemText>
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link style={linkStyle} to="/all-recipes">
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText>ALL RECIPES</ListItemText>
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link style={linkStyle} to="/about">
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText>ABOUT</ListItemText>
+            </ListItemButton>
+          </ListItem>
+        </Link>
 
+        <Link style={linkStyle} to="/logIn">
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText>LOG IN</ListItemText>
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      </List>
+    </Box>
+  );
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
   return (
-    <div className="navbar-container">
-      <div className="navbar-left">
-        <Link to="/">Home</Link>
-        <Link to="/all-recipes">All Recipes</Link>
-        <Link to="/about">About</Link>
-      </div>
+    <Box>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <MenuIcon />
+        </IconButton>
 
-      <div className="navbar-center">
-        <Link to="/">
-          <img src={Logo} alt="logo" />
-        </Link>
-      </div>
-      <div className="navbar-right">
-        <Link to="/favorites">
-          <StyledBadge badgeContent={favState.length} color="error">
-            <IconButton>
-              <FavoriteBorderIcon />
-            </IconButton>
-          </StyledBadge>
-        </Link>
-        <Link to={isLogin ? "/" : "/registration"}>
-          {isLogin ? "Welcome to Food Blog" : "Register"}
-        </Link>
-        <Link to={isLogin ? "/user" : "/login"}>
-          <StyledBadge badgeContent={user.isAdmin && isLogin ? "A" : null} color="info">
-            <LoginBTN>{isLogin ? user.firstName : "Log In"}</LoginBTN>
-          </StyledBadge>
-        </Link>
-      </div>
-    </div>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            width: "100%",
+          }}
+        >
+          <Box
+            sx={{
+              display: {
+                xs: "none",
+                sm: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+                height: "100px",
+                width: "100%",
+              },
+            }}
+          >
+            <Link style={linkStyle} to="/">
+              HOME
+            </Link>
+            <Link style={linkStyle} to="/all-recipes">
+              ALL RECIPES
+            </Link>
+            <Link style={linkStyle} to="/about">
+              ABOUT
+            </Link>
+          </Box>
+          <Box
+            className="navbar-center"
+            sx={{
+              display: {
+                xs: "none",
+                sm: "flex",
+                justifyContent: "space-around",
+                width: "50%",
+              },
+            }}
+          >
+            <Link style={linkStyle} to="/">
+              <img
+                src={Logo}
+                alt="logo"
+                style={{
+                  width: "100px",
+                  height: "100px",
+                }}
+              />
+            </Link>
+          </Box>
+          <Box
+            className="navbar-right"
+            sx={{
+              display: {
+                xs: "none",
+                sm: "flex",
+                justifyContent: "space-around",
+
+                width: "100%",
+                alignItems: "center",
+                height: "100px",
+              },
+            }}
+          >
+            <Link style={linkStyle} to="/favorites">
+              <StyledBadge badgeContent={favState.length} color="error">
+                <IconButton>
+                  <FavoriteBorderIcon />
+                </IconButton>
+              </StyledBadge>
+            </Link>
+            <Link style={linkStyle} to={isLogin ? "/" : "/registration"}>
+              {isLogin ? "Welcome to Food Blog" : "REGISTER"}
+            </Link>
+            <Link style={linkStyle} to={isLogin ? "/user" : "/login"}>
+              <StyledBadge
+                badgeContent={user.isAdmin && isLogin ? "A" : null}
+                color="info"
+              >
+                <LoginBTN>{isLogin ? user.firstName : "LOG IN"}</LoginBTN>
+              </StyledBadge>
+            </Link>
+          </Box>
+        </Box>
+      </Toolbar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
   );
 }
 
