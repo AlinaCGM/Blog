fdcbd173a417fb2823260e15abb7cb99edc57848
Alina <alinbogz@gmail.com>
2023-08-23 22:12:04 +0200

    cors updated back

--- api/src/app.ts
+++ api/src/app.ts
@@ -8,13 +8,7 @@ import { jwtStrategy, googleStrategy } from "./config/passport";
 
 const app = Express();
 app.use(Express.json());
-app.use(
-  cors({
-    origin: "https://blog-client-wz90.onrender.com",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // Allow sending cookies with requests
-  })
-);
+app.use(cors());
 app.use(passport.initialize());
 passport.use(jwtStrategy);
 passport.use(googleStrategy);
